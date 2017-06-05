19f3416ef86576fca34834efd7474fefeaece552
Rolf <rolf.ebert.gcc@gmx.de>
2017-06-05 23:05:57 +0200

    new client mqtt2influx. The program subscribes to various topics and writes the received values to the Influx time series database.

---
+++ src/mqtt2influx.adb
@@ -0,0 +1,141 @@
+with Ada.Exceptions;               use Ada.Exceptions;
+with Ada.Text_IO;
+with Ada.Real_Time;
+
+with Alog;                         use Alog;
+with Alog.Logger;
+with Alog.Policy_DB;
+with Alog.Facilities.File_Descriptor;
+with Logs;                         use Logs;
+
+with GNAT.Sockets.MQTT;            use GNAT.Sockets.MQTT;
+with GNAT.Sockets.MQTT.Server;     use GNAT.Sockets.MQTT.Server;
+with GNAT.Sockets.Server;          use GNAT.Sockets.Server;
+with GNAT.Sockets.Server.Handles;  use GNAT.Sockets.Server.Handles;
+with Strings_Edit.Integers;        use Strings_Edit.Integers;
+
+with MQTT_Influx_Client;           use MQTT_Influx_Client;
+
+with Config;                       use Config;
+with Influx;
+
+
+procedure MQTT2Influx is
+   --  read config file(s).
+   Cfg : Configuration := Init ("mqtt2influx.cfg",
+                                Case_Sensitive   => False,
+                                On_Type_Mismatch => Print_Warning);
+
+
+   My_Version  : constant String  := "V0.0";
+   My_Name     : constant String  := Config.Value_Of (Cfg, "broker", "client_id", "mqtt2influx");
+   Broker_Name : constant String  := Config.Value_Of (Cfg, "broker", "servername", "localhost");
+   Port_Int    : constant Integer := Config.Value_Of (Cfg, "broker", "port", 1883);
+   Broker_Port : constant GNAT.Sockets.Port_Type := GNAT.Sockets.Port_Type (Port_Int);
+
+
+   Influxserver_Default : constant String :=
+     Config.Value_Of (Cfg, "defaults", "influxdbserver", "localhost");
+   Influxport_Default : constant Natural :=
+     Config.Value_Of (Cfg, "defaults", "influxdbport", 8086);
+   Influxdb_Default : constant String :=
+     Config.Value_Of (Cfg, "defaults", "influxdb", "testdata_db");
+
+
+   Factory   : aliased Connections_Factory;
+   Broker    : aliased Connections_Server (Factory'Access, 0);
+   QoS       : constant QoS_Level := At_Most_Once;
+   Myself    : aliased MQTT_Client (Listener             => Broker'Unchecked_Access,
+                                    Input_Size           => 800,
+                                    Output_Size          => 800,
+                                    Max_Subscribe_Topics => 500);
+
+   Sub_Id : Packet_Identification (QoS);
+   Sub_Nr : Packet_Identifier := 24; -- arbitrary, may be anything
+
+   use Config;
+   use Config.String_Vector;
+
+   Patterns : Section_List := Read_Sections (Cfg);
+
+   procedure Remove_Section (Name : String)
+   is
+      Position : Cursor;
+   begin
+      Position := Find (Patterns, Name);
+      Delete (Patterns, Position);
+   exception
+   when Constraint_Error => null;
+   --  one of the predefined sections doesn't exist, don't care
+   end Remove_Section;
+
+begin
+   L.Log_Message (Debug, "MQTT2Influx started (MQTT client id = '" & My_Name & " " & My_Version & "')");
+
+   --  read configured patterns
+   Remove_Section ("broker");
+   Remove_Section ("logs");
+   Remove_Section ("defaults");
+   L.Log_Message (Debug, "default Influx server  = " & Influxserver_Default);
+   L.Log_Message (Debug, "default Influx port    = " & Image(Influxport_Default));
+   L.Log_Message (Debug, "default Influx DB name = " & Influxdb_Default);
+
+   for P of Patterns loop
+      Influx.Influxserv.Insert (P, Config.Value_Of (Cfg, P, "influxdbserver", Influxserver_Default));
+      L.Log_Message (Debug, "influxserver("&P&")="&Influx.Influxserv(P));
+      Influx.Influxport.Insert (P, GNAT.Sockets.Port_Type (Config.Value_Of (Cfg, P, "influxdbport", Influxport_Default)));
+      -- L.Log_Message (Debug, "influxport("&P&")="&Image(Natural(Influx.Influxport(P))));
+      Influx.Influxdb.Insert (P, Config.Value_Of (Cfg, P, "influxdb", Influxdb_Default));
+      L.Log_Message (Debug, "influxdb("&P&")="&Influx.Influxdb(P));
+      Influx.Influxname.Insert (P, Config.Value_Of (Cfg, P, "influxname", P));
+      L.Log_Message (Debug, "influxname("&P&")="&Influx.Influxname(P));
+   end loop;
+
+
+   if Policy_DB.Get_Default_Loglevel = Debug then
+      Trace_On (Factory  => Factory,
+                Received => GNAT.Sockets.Server.Trace_Decoded,
+                Sent     => GNAT.Sockets.Server.Trace_Decoded);
+   else
+      Trace_Off (Factory);
+   end if;
+
+   --
+   --  connect to broker
+   --
+   if QoS > At_Least_Once then
+      Sub_Id.ID := 24; -- arbitrary
+   end if;
+
+   Set_Overlapped_Size (Myself, 4); -- One response packet (4 bytes) is
+                                    -- queued for send without blocking
+                                    -- receiving
+
+   L.Log_Message (Debug, "connect to MQTT broker " & Broker_Name & ':' & Broker_Port'Img);
+   Connect (Broker, Myself'Unchecked_Access, Broker_Name, Broker_Port);
+   while not Is_Connected (Myself) loop -- busy waiting
+      delay 0.01;
+   end loop;
+   L.Log_Message (Debug, My_Name & " connected to MQTT broker " & Broker_Name & ':' & Broker_Port'Img);
+   Send_Connect (Myself, My_Name);
+
+   delay 0.1;
+
+   --  subscribe to patterns
+   for P of Patterns loop
+      L.Log_Message (Info, "subscribe to " & P);
+      Send_Subscribe (Myself, Sub_Nr, P, QoS);
+   end loop;
+
+   loop
+      --  wait for incoming message
+      --  on reception handle as configured
+      --  send the transformed message to influx
+      delay 0.01;
+   end loop;
+
+exception
+when E : others =>
+   L.Log_Message (Error, "Error (main): " & Exception_Information (E));
+
+end MQTT2Influx;
---
+++ src/mqtt_influx_client.adb
@@ -0,0 +1,155 @@
+--                                                                    --
+--  package MQTT_Clients            Copyright (c)  Dmitry A. Kazakov  --
+--  Implementation                                 Luebeck            --
+--                                                 Spring, 2016       --
+--                                                                    --
+--                                Last revision :  18:59 21 Mar 2016  --
+--                                                                    --
+--  This  library  is  free software; you can redistribute it and/or  --
+--  modify it under the terms of the GNU General Public  License  as  --
+--  published by the Free Software Foundation; either version  2  of  --
+--  the License, or (at your option) any later version. This library  --
+--  is distributed in the hope that it will be useful,  but  WITHOUT  --
+--  ANY   WARRANTY;   without   even   the   implied   warranty   of  --
+--  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU  --
+--  General  Public  License  for  more  details.  You  should  have  --
+--  received  a  copy  of  the GNU General Public License along with  --
+--  this library; if not, write to  the  Free  Software  Foundation,  --
+--  Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.    --
+--                                                                    --
+--  As a special exception, if other files instantiate generics from  --
+--  this unit, or you link this unit with other files to produce  an  --
+--  executable, this unit does not by  itself  cause  the  resulting  --
+--  executable to be covered by the GNU General Public License. This  --
+--  exception  does not however invalidate any other reasons why the  --
+--  executable file might be covered by the GNU Public License.       --
+--____________________________________________________________________--
+
+-- 2016-12: R. Ebert, use alog instead of Text_IO
+
+with Ada.Exceptions;
+with Ada.Strings.Bounded;
+with Ada.Calendar; -- Real_Time;
+with Ada.Calendar.Formatting;
+
+with Alog;                         use Alog;
+with Alog.Logger;                  use Alog.Logger;
+with Logs;                         use Logs;
+
+with GNAT.Sockets;                 use GNAT.Sockets;
+--  with GNAT.Sockets.Server;          use GNAT.Sockets.Server;
+--  with GNAT.Sockets.Server.Handles;  use GNAT.Sockets.Server.Handles;
+with Strings_Edit.Integers;        use Strings_Edit.Integers;
+--  with GNAT.Sockets.Connection_State_Machine.HTTP_Server;
+--  with GNAT.Sockets.Connection_State_Machine.HTTP_Client.Signaled;
+--  use  GNAT.Sockets.Connection_State_Machine.HTTP_Client.Signaled;
+with Strings_Edit.Streams;
+--  with Generic_FIFO;
+
+with Influx;
+
+
+package body MQTT_Influx_Client is
+
+
+   procedure On_Connect_Accepted (Pier            : in out MQTT_Client;
+                                  Session_Present : Boolean)
+   is
+   begin
+      L.Log_Message (Info, "MQTT connect accepted");
+   end On_Connect_Accepted;
+
+
+   procedure On_Connect_Rejected (Pier     : in out MQTT_Client;
+                                  Response : Connect_Response)
+   is
+   begin
+      L.Log_Message (Warning, "Connect rejected " & Image (Response));
+   end On_Connect_Rejected;
+
+
+   procedure On_Publish (Pier      : in out MQTT_Client;
+                         Topic     : String;
+                         Message   : Stream_Element_Array;
+                         Packet    : Packet_Identification;
+                         Duplicate : Boolean;
+                         Retain    : Boolean)
+   is
+      use Strings_Edit;
+      use Strings_Edit.Integers;
+      use Strings_Edit.Streams;
+
+      use Influx;
+
+      To_Influx : Socket_Type;
+      Address : Sock_Addr_Type;
+      Channel : GNAT.Sockets.Stream_Access;
+
+      Output   : String (1 .. 500);
+      Content  : String (1 .. 400);
+      Out_Ptr  : Natural         := 1;
+      Cont_Ptr : Natural         := 1;
+      CRLF     : constant String := ASCII.CR & ASCII.LF;
+   begin
+      L.Log_Message (Info, "received " & Topic & " = " ); -- & Image (Message));
+      On_Publish (MQTT_Pier (Pier), Topic, Message, Packet, Duplicate, Retain);
+
+      GNAT.Sockets.Initialize;  -- initialize a specific package
+      Create_Socket (To_Influx);
+      Set_Socket_Option (To_Influx, Socket_Level, (Reuse_Address, True));
+      Address.Addr := Addresses (Get_Host_By_Name (Influxserv(Topic)), 1);
+      Address.Port := Influxport(Topic);
+
+      Connect_Socket (To_Influx, Address);
+      L.Log_Message (Info, "connected to influx" );
+      Channel := Stream (To_Influx);
+
+      declare
+         use Strings_Edit;
+         Port : constant Natural := Natural(Port_Type'(Influxport(Topic)));
+         Msg_Str : String (1 .. Message'Length);
+         for Msg_Str'Address use Message'Address;
+      begin
+         L.Log_Message (Info, "Message = '"&Msg_Str&''');
+
+         Put (Output, Out_Ptr, "POST /write?db=");
+         Put (Output, Out_Ptr, Influxdb(Topic));
+         Put (Output, Out_Ptr, " HTTP/1.1");
+         Put (Output, Out_Ptr, CRLF);
+         Put (Output, Out_Ptr, "Host: ");
+         Put (Output, Out_Ptr, Influxserv(Topic));
+         Put (Output, Out_Ptr, ':');
+         Put (Output, Out_Ptr, Image(Port));
+         Put (Output, Out_Ptr, CRLF);
+         Put (Output, Out_Ptr, "Accept: */*");
+         Put (Output, Out_Ptr, CRLF);
+         Put (Output, Out_Ptr, "Content-Type: application/x-www-form-urlencoded");
+         Put (Output, Out_Ptr, CRLF);
+         Put (Output, Out_Ptr, "Content-Length: ");
+
+         Put (Content, Cont_Ptr, Influxname(Topic));
+         Put (Content, Cont_Ptr, " value=");
+         Put (Content, Cont_Ptr, Msg_Str);
+         Cont_Ptr := Cont_Ptr - 1;
+         L.Log_Message (Info, "Content = '"&Content(1..Cont_Ptr)&''');
+
+         Put (Output, Out_Ptr, Image (Cont_Ptr));
+         Put (Output, Out_Ptr, CRLF);
+         Put (Output, Out_Ptr, CRLF);
+         Put (Output, Out_Ptr, Content(1..Cont_Ptr));
+
+         Out_Ptr := Out_Ptr - 1;
+         String'Write (Channel, Output(1..Out_Ptr));
+         L.Log_Message (Info, "sent '"&Output(1..Out_Ptr)&''');
+      end;
+      Close_Socket (To_Influx);
+      L.Log_Message (Info, "closed Influx socket" );
+
+
+   exception
+   when E : others =>
+      L.Log_Message (Error, "(On_Publish)" & Ada.Exceptions.Exception_Information (E));
+      raise;
+   end On_Publish;
+
+end MQTT_Influx_Client;
